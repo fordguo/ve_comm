03f7c53ebeebee36832a319123f4f62ab1d41b95
Ford Guo <agile.guo@qq.com>
2021-01-19 15:02:01 +0800

    change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ve-comm',
-    version='0.2.1',
+    version='0.2.2',
     description='Virtual meeting common package',
     author='Ford Guo',
     author_email='agile.guo@qq.com',
