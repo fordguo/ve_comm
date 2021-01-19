f512a9536a104cf24f588704340f584be0c635ce
Ford Guo <agile.guo@qq.com>
2021-01-19 15:10:50 +0800

    include html

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(
         include=['webinar', 'webinar.*', 'ctools', 'ctools.*',
                  'cmedia', 'cmedia.=*']),
-    include_package_data=True,
+    package_data={'': ['*.html']},
     install_requires=[
         'django>=3.1',
         'wagtail>=2.11',
