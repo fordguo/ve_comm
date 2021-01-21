ccd00319c6fa0d92d12e5ede7ccef396e1eb74df
Ford Guo <agile.guo@qq.com>
2021-01-21 16:12:41 +0800

    change templates

--- cpage/views.py
+++ cpage/views.py
@@ -7,5 +7,10 @@ class AdaptiveTemplateMixin:
     def get_template_names(self):
         names = super().get_template_names()
         if not self.request.user_agent.is_pc:
-            names[0] = f"mobile_{names[0]}"
+            template = names[0]
+            idx = template.rfind("/")
+            if idx != -1:
+                names[0] = f"{template[:idx]}/mobile_{template[idx+1:]}"
+            else:
+                names[0] = f"mobile_{template}"
         return names
