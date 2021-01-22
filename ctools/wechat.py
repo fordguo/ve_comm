09785c70eff3790fab2cb3301dd98f6d9705b60c
Ford Guo <agile.guo@qq.com>
2021-01-22 14:34:32 +0800

    use WECHAT prefix

--- ctools/wechat.py
+++ ctools/wechat.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 def redirect_wechat(request):
     # TODO: build_absolute_uri，对于https的url，返回的是http
     # https://djangodeployment.com/2017/01/24/fix-djangos-https-redirects-nginx/ 这个配置无效，可能跟SLB有关
-    url = f'{settings.WEIXIN_AUTH_URL}?appid={settings.WEIXIN_APPID}&target={request.build_absolute_uri()}'
+    url = f'{settings.WECHAT_H5_URL}?appid={settings.WECHAT_APPID}&target={request.build_absolute_uri()}'
     print(url)
     return redirect(url)
 
