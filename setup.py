4bd719b8524139248f2cf4f286b1f277ac4fa583
Ford Guo <agile.guo@qq.com>
2021-01-26 14:15:25 +0800

    release 1.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ve-comm',
-    version='1.3.3',
+    version='1.3.4',
     description='Virtual meeting common package',
     author='Ford Guo',
     author_email='agile.guo@qq.com',
