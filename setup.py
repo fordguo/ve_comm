e3d62c30ee4c11b318df77137eff6dcee7b32947
Ford Guo <agile.guo@qq.com>
2021-02-22 17:15:39 +0800

    change to 1.3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ve-comm',
-    version='1.3.6',
+    version='1.3.7',
     description='Virtual meeting common package',
     author='Ford Guo',
     author_email='agile.guo@qq.com',
