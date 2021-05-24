9905a6ef4b2f5d90c06b6890e9b52923a5d84fe0
Ford Guo <agile.guo@qq.com>
2021-05-24 17:04:11 +0800

    fixes avator

--- ctools/avatar.py
+++ ctools/avatar.py
@@ -11,6 +11,21 @@ def _check_black_name(name):
     return name
 
 
+def _check_single_name(names):
+    if len(names) > 1:
+        return f"{names[0][0][0]}{names[1][0][0]}"
+    elif len(names) == 1 and len(names[0][0]) >= 1:
+        nm = names[0][0].strip()
+        sn = nm.split(' ')
+        if len(sn) > 1:
+            return f"{sn[0][0]}{sn[1][0]}"
+        n = nm[0:2]
+        if len(n) == 1:
+            n = f"{n}X"
+        return n
+    return _DEFAULT_NAME
+
+
 def _avatar_name(first_name, last_name, email):
     # 李: [['l']]
     # Bruce Lee: [['Bruce Lee']]
@@ -21,12 +36,10 @@ def _avatar_name(first_name, last_name, email):
         return f"{first[0][0][0]}{last[0][0][0]}"
     elif first_name:
         first = pinyin(first_name, style=Style.FIRST_LETTER)
-        if len(first) > 1:
-            return f"{first[0][0][0]}{first[0][1][0]}"
+        return _check_single_name(first)
     elif last_name:
         last = pinyin(last_name, style=Style.FIRST_LETTER)
-        if len(last) > 1:
-            return f"{last[0][0][0]}{last[0][1][0]}"
+        return _check_single_name(last)
     elif email:
         names = email.split('@')[0].split('.')
         if len(names) > 1:
@@ -44,3 +57,11 @@ def get_avatar_url(user):
 
 def get_default_avatar(user_id):
     return f'{OSS_URL_PREFIX}/avatar/{_DEFAULT_NAME}_{user_id % 3}.png'
+
+
+if __name__ == "__main__":
+    print(_avatar_name('Xs', None, None))
+    print(_avatar_name('测试', None, None))
+    print(_avatar_name('X ', None, None))
+    print(_avatar_name('', None, None))
+    print(_avatar_name('John Ford', None, None))
