709d8803b0d6432914c55f6ef8f2504cca65997b
Ford Guo <agile.guo@qq.com>
2021-07-14 11:17:43 +0800

    move isascii to utils

--- ctools/__init__.py
+++ ctools/__init__.py
@@ -1,4 +1,1 @@
-
-def isascii(s):
-    """Check if the characters in string s are in ASCII, U+0-U+7F."""
-    return len(s) == len(s.encode())
+from .utils import isascii
--- ctools/utils.py
+++ ctools/utils.py
@@ -4,3 +4,8 @@ def get_email_name(email):
     if len(names) > 1:
         return (names[0], ' '.join(names[1:]))
     return (names[0], '')
+
+
+def isascii(s):
+    """Check if the characters in string s are in ASCII, U+0-U+7F."""
+    return len(s) == len(s.encode())
