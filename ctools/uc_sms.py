6e1b08dc4beb738fe056aa5223a4ebf0b501abf3
Ford Guo <agile.guo@qq.com>
2021-08-12 11:33:07 +0800

    add uc_sms.py

---
+++ ctools/uc_sms.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from django.conf import settings
+
+from .models import SmsLog
+# 云极短信服务
+
+ROOT_URL = "https://market.juncdt.com/smartmarket/"
+
+
+def send_sms(phones, sign_code, template_code, classification, **params):
+    url = f"{ROOT_URL}msgService/sendMessageToMulti"
+    main = settings.UC['main']
+    jdata = {"accessKey": main['accessKey'],
+             "accessSecret": main['accessSecret'],
+             "classificationSecret": classification,
+             "signCode": sign_code, "templateCode": template_code,
+             "phones": phones, "params": params,
+             }
+    res = requests.post(url, json=jdata)
+    strphones = ','.join(phones)
+    strparams = json.dumps(params)
+    SmsLog.objects.create(
+        phones=strphones, sign_name=sign_code, template=template_code, params=strparams, send_result=res.text)
+
+
+def send_internation_sms(phones, template_code, **params):
+    url = f"{ROOT_URL}internationalMsg/sendMsgToMultiple"
+    inter = settings.UC['internation']
+    jdata = {"accessKey": inter['accessKey'],
+             "accessSecret": inter['accessSecret'],
+             "templateCode": template_code,
+             "phones": phones, "params": params,
+             }
+    res = requests.post(url, json=jdata)
+    strphones = ','.join(phones)
+    strparams = json.dumps(params)
+    SmsLog.objects.create(
+        phones=strphones, sign_name='', template=template_code, params=strparams, send_result=res.text)
